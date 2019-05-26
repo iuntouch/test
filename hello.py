f4a7c38987da2f199e7a002a400e9297a2506872
iuntouch <40690911+iuntouch@users.noreply.github.com>
2019-05-26 23:56:55 +0800

    flask扩展

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import request
-
+from flask_script import Manager
 
 app = Flask(__name__)
-
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -18,4 +18,5 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
