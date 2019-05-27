aee4e48fee2f2ab4c9b768d6e15b2c1e9ff6cc51
iuntouch <40690911+iuntouch@users.noreply.github.com>
2019-05-27 17:04:57 +0800

    模板渲染， 控制语句以及继承

--- hello.py
+++ hello.py
@@ -1,22 +1,24 @@
-from flask import Flask
-from flask import request
-from flask_script import Manager
+from flask import Flask, render_template
+# from flask import request
+# from flask_script import Manager
 
 app = Flask(__name__)
-manager = Manager(app)
+# manager = Manager(app)
 
 @app.route('/')
 def index():
     # return '<h1>Hello World!</h1>'
-    user_agent = request.headers.get('User_Agent')
-    return '<p>Your brower is {}!</p>'.format(user_agent)
+    # user_agent = request.headers.get('User_Agent')
+    # return '<p>Your brower is {}!</p>'.format(user_agent)
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    # return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()
+    app.run(debug=True)
+    # manager.run()
