a58fe1ae0a5f93d5e58ca98c13ed207e6f60f539
iuntouch <40690911+iuntouch@users.noreply.github.com>
2019-05-29 17:41:58 +0800

    表单类及处理

--- hello.py
+++ hello.py
@@ -1,21 +1,44 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 # from flask import request
 # from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NameForm(FlaskForm):
+    name = StringField('What"s your name?', validators=[DataRequired()])
+    sumbit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 # manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        # name = form.name.data
+        # form.name.data = ''
+        # session['name'] = form.name.data
+        # return redirect(url_for('index'))
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     # return '<h1>Hello World!</h1>'
     # user_agent = request.headers.get('User_Agent')
     # return '<p>Your brower is {}!</p>'.format(user_agent)
-    return render_template('index.html', current_time=datetime.utcnow())
+    # return render_template('index.html', current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
