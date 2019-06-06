81b65dd7724bad9bbf2c4b11f6c7396ad2d28685
iuntouch <40690911+iuntouch@users.noreply.github.com>
2019-06-06 15:51:13 +0800

    数据库及其迁移

--- hello.py
+++ hello.py
@@ -1,12 +1,57 @@
+import os
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
 # from flask import request
-# from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(FlaskForm):
@@ -14,11 +59,10 @@ class NameForm(FlaskForm):
     sumbit = SubmitField('Submit')
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-# manager = Manager(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+# @app.shell_context_processor
+# def make_shell_context():
+#     return dict(db=db, User=User, Role=Role)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,16 +73,26 @@ def index():
         # form.name.data = ''
         # session['name'] = form.name.data
         # return redirect(url_for('index'))
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+
+        # old_name = session.get('name')
+        # if old_name is not None and old_name != form.name.data:
+        #     flash('Looks like you have changed your name!')
+        # session['name'] = form.name.data
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
     # return '<h1>Hello World!</h1>'
     # user_agent = request.headers.get('User_Agent')
     # return '<p>Your brower is {}!</p>'.format(user_agent)
     # return render_template('index.html', current_time=datetime.utcnow())
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow(), known = session.get('known',False))
 
 
 @app.route('/user/<name>')
@@ -58,5 +112,5 @@ def internal_server_error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # manager.run()
+    # app.run(debug=True)
+    manager.run()
